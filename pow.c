03562751434aee0506af454325ef8e80c0f3c466
kathirvelchitra <31887330+kathirvelchitra@users.noreply.github.com>
2018-02-02 14:24:58 +0530

    Create pow.c

---
+++ pow.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the value:");
+scanf("%d",&n);
+if(n%2==0)
+{
+    printf("it is a power of 2");
+}
+else
+{ 
+    printf("not a power of 2");
+}
+}
