f09ea80058421c261dc7b8c1a5238165fbf91d6f
kathirvelchitra <31887330+kathirvelchitra@users.noreply.github.com>
2018-02-02 14:42:52 +0530

    Create kn.c

---
+++ kn.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+ int a,b,c,d;
+ printf("enter a value");
+ scanf("%d",&a);
+ printf("enter b value");
+ scanf("%d",&b);
+ printf("enter c value");
+ scanf("%d",&c);
+ d=a+b+c;
+ printf("%d",d);
+}
