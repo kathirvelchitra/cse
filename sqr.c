a0c838f675afe83bd6913196f5323fd471f44f35
kathirvelchitra <31887330+kathirvelchitra@users.noreply.github.com>
2018-02-27 16:50:29 +0530

    Create sqr.c

---
+++ sqr.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("enter the num \n");
+    scanf("%d %d",&a,&b);
+    c=a*b;
+    if(a==b)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
+
