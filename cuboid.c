e589ae04eef9f4096e84c5f73d59cb32955bd8d9
kathirvelchitra <31887330+kathirvelchitra@users.noreply.github.com>
2018-03-02 14:05:54 +0530

    Create cuboid.c

---
+++ cuboid.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+int main()
+{
+    
+   int l,b,h,t;
+   printf("enter the 3 values \n");
+   scanf("%d%d%d",&l,&b,&h);
+   t=l*b*h;
+   printf("\n %d",t);
+    return 0;
+    
+}
+
+
+
