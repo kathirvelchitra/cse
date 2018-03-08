d5980baf763bd8487a6c59e2f8c99498975f412a
kathirvelchitra <31887330+kathirvelchitra@users.noreply.github.com>
2018-03-08 16:15:51 +0530

    Create abc.c

---
+++ abc.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int A,B,C,D;
+    printf("Enter the A,B,C value");
+    scanf("%d %d %d",&A,&B,&C);
+    D=(A*B)/C;
+    printf("%d",D);
+    return 0;
+}
