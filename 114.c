3efb64f3088a40247d60870e78fafc372355db0a
kathirvelchitra <31887330+kathirvelchitra@users.noreply.github.com>
2018-03-20 16:06:34 +0530

    Create 114.c

---
+++ 114.c
@@ -0,0 +1,24 @@
+
+#include<stdio.h>
+int main()
+{
+	int n,k,b[100],i,value=0;
+	scanf("%d %d",&n,&k);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&b[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+  if(b[i]%2!=0)
+	{
+		value++;
+		if(value==2)
+		{
+			printf("%d",b[i]);
+			return(0);
+		}
+	}
+	}
+	return(0);	
+}
